63e38af049cfa79eae5e5f7ab3cbc7ebcfde49b9
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-04-02 20:58:05 -0600

    some configs

--- adventure/models.py
+++ adventure/models.py
@@ -12,13 +12,6 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
-    x = 0
-    y = 0
-    
-    
-
-    
-    
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
@@ -68,6 +61,7 @@ def create_user_player(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_player(sender, instance, **kwargs):
     instance.player.save()
+
 
 
 
